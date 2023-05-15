408418414a1f0cc6282f22cb57b9add4c08b6397
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-15 13:46:15 -0300

    add cart drawer

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,30 +1,44 @@
-'use client'; 
+"use client";
 
-import { useState } from 'react';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import styles from '@/app/styles/cart.module.css'
-
+import { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import styles from "@/app/styles/cart.module.css";
 
 const Cart = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+  const [isAnimationDone, setIsAnimationDone] = useState(false);
 
   const handleOpenCart = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleCloseCart = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsOpen(false);
+      setIsClosing(false);
+      setIsAnimationDone(true);
+    }, 500);
   };
 
   return (
     <div>
       <AiOutlineShoppingCart onClick={handleOpenCart} />
       {isOpen && (
-        <div className={styles.cart_container}>
+        <div
+          className={`${styles.cart_container} ${isClosing && styles.closing}`}
+          onAnimationEnd={() => setIsAnimationDone(false)}
+        >
           <div className={styles.cart_header}>
             <h1>Seu Carrinho</h1>
-            <AiOutlineShoppingCart onClick={handleOpenCart} />
+            <AiOutlineShoppingCart onClick={handleCloseCart} />
           </div>
         </div>
       )}
+      {!isOpen && isAnimationDone && null}
     </div>
-  )
+  );
 };
 
 export default Cart;
