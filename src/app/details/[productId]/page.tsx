6bffadf0dd350ceadb148c08be652f85a704d520
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-18 01:07:33 -0300

    add oldpriice

--- src/app/details/[productId]/page.tsx
+++ src/app/details/[productId]/page.tsx
@@ -1,40 +1,77 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { allProducts } from "@/app/data/data";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 import ProductNotFound from "@/app/components/ProductNotFound";
 
+import styles from "@/app/styles/productDetails.module.css";
+
 export default function Home() {
-  const pathname = usePathname()
-  const product = pathname.split('/')[2]
-  const productFiltered = decodeURIComponent(product)
+  const pathname = usePathname();
+  const product = pathname.split("/")[2];
+  const productFiltered = decodeURIComponent(product);
   const productData = allProducts.find((item) => item.name === productFiltered);
 
   return (
     <main>
-    <section>
-      {productData ? (
-        <div>
-          <div>
-            <Image
-              src={productData.image}
-              alt={productData.name}
-              width={400}
-              height={400}
-            />
+      <section className={styles.details_container}>
+        {productData ? (
+          <div className={styles.details_wrapper}>
+            <div>
+              <Image
+                src={productData.image}
+                alt={productData.name}
+                width={330}
+                height={260}
+                className={styles.details_image}
+              />
+            </div>
+            <div className={styles.details}>
+              <h1>{productData.name}</h1>
+              <span>R${productData.price}</span>
+              {productData.oldPrice && (
+                <span className={styles.details_oldPrice}>
+                  R${productData.oldPrice}
+                </span>
+              )}
+              <p>{productData.details}</p>
+              <div>
+                <div>
+                  <label htmlFor="">Quantidade</label>
+                  <button>+</button>
+                  <span>0</span>
+                  <button>-</button>
+                </div>
+                <div>
+                  <span>Inclui vaso</span>
+                  <select name="" id="">
+                    <option value="">Não</option>
+                    <option value="">Sim</option>
+                  </select>
+                </div>
+              </div>
+              <button>Adicionar ao Carrinho</button>
+              <div className={styles.details_delivery}>
+              <h2>Entrega</h2>
+              <p>
+                <span>Frete grátis</span> para São Paulo em compras acima de
+                R$200.00
+              </p>
+              <div>
+                <span>Calcular frete</span>
+                <input type="text" placeholder="00000-000" />
+                <button>OK</button>
+                <p>{}</p>
+              </div>
+            </div>
+            </div>
           </div>
-          <div>
-            <h1>{productData.name}</h1>
-            <p>{productData.details}</p>
-            <p>{productData.price}</p>
-          </div>
-        </div>
-      ) : (
-        <ProductNotFound />
-      )}
-    </section>
-  </main>
-  )
+        ) : (
+          <ProductNotFound />
+        )}
+      </section>
+    </main>
+  );
 }
